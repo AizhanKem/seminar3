927ff347480bfa8cc93544fb344cc612edcce1be
AizhanKem <aizhankem@gmail.com>
2019-03-09 11:15:22 +0600

    ..

---
+++ ds.py
@@ -0,0 +1,41 @@
+dict = {}
+key = "counter"
+if key not in dict:
+    dict[key]=0
+dict[key] += 1
+from collections import defaultdict
+dict = defaultdict(int)
+dict['counter']+=1
+q = []
+q.insert[0,v]
+q.pop()
+stack = []
+stack.append(v)
+stack.pop()
+from collections import deque
+fifo=deque()
+fifo.append(l)
+fifo.popleft()
+lifo = deque()
+lifo.append(l)
+lifo.pop()
+A[PARENT(i)]>= A[i];
+heap = []
+heappush(heap, 5)
+heappush(heap, 3)
+heappush(heap, 7)
+heappush(heap, 4)
+print(heappop(a), heappop(a), heappop(a), heappop(a))
+assert heap[0] == nsmallest(1, heap)[0] == 3
+
+class ListNode(object):
+     def __init__(self, x):
+         self.val = x
+         self.next = None
+
+pointer=head = ListNode(None)
+
+while pointer:
+    print pointer.val
+    pointer = pointer.next
+
